fb347625e51708a796b0dd9963c8e5838d359096
KK <karanjakelvin77@gmail.com>
2022-04-05 07:22:37 +0300

    It will add the Profile and Image models

--- doubletap/tap/models.py
+++ doubletap/tap/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    id = models.CharField(primary_key=True, max_length=20)
+    profile_photo = models.CharField(max_length=30)
+    bio = models.CharField(max_length=30)
+    
+class Image(models.Model):
+    id = models.CharField(primary_key=True,max_length=20)
+    image = models.CharField(max_length=30)
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=30)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.BooleanField()
+    comments = models.CharField(max_length=30) 
