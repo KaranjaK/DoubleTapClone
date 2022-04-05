60aca1b280d1736b6d1057cc8778c8d66b4cb122
KK <karanjakelvin77@gmail.com>
2022-04-05 13:33:13 +0300

    It will test the models

--- doubletap/tap/models.py
+++ doubletap/tap/models.py
@@ -1,11 +1,18 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=30)
     profile_photo = models.CharField(max_length=30)
     bio = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
     
+
 class Image(models.Model):
     id = models.CharField(primary_key=True,max_length=20)
     image = models.CharField(max_length=30)
@@ -14,3 +21,12 @@ class Image(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     likes = models.BooleanField()
     comments = models.CharField(max_length=30) 
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete()
--- doubletap/tap/tests.py
+++ doubletap/tap/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Profile, Image
 
 # Create your tests here.
+
+# Profile Tests
+class ProfileTest(TestCase):
+    
+    def setUp(self):
+        self.new_user = Profile(2,'Msee','Null','Freelancer')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_user, Profile))
+
+# Image Tests
+class ImageTest(TestCase):
+
+    def setUp(self):
+        self.my_image = Image(9,'Not Found', 'My Prof','That is more like it', 'Yea', 20, 'So nice')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.my_image, Image))
