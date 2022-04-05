156f70739169150759d860f18c5e3819e51b7d5d
KK <karanjakelvin77@gmail.com>
2022-04-05 06:34:41 +0300

    It will add the database and make the first migration

--- doubletap/doubletap/settings.py
+++ doubletap/doubletap/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'doubletap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'doubletap',
+        'USER': 'kk',
+        'PASSWORD': 'admin101'
+
+
     }
 }
 
